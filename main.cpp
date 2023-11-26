542ee7455fb622ba76064588e1118644ad952451
mudit-51 <mudit.7.gupta+github@gmail.com>
2023-11-26 16:25:26 +0530

    implemented write-through

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int NUM_SETS;
 int OFFSET_WIDTH;
 int INDEX_WIDTH;
 int TAG_WIDTH;
+int WRITE_HIT;
+int WRITE_MISS;
 
 int hex_to_int(string hex) {
     int x;
@@ -58,6 +60,14 @@ vector<string> retrieve_from_mem(map<string, vector<string>>& mem,
     return data;
 }
 
+void write_to_mem(map<string, vector<string>>& mem, string hex_address,
+                  string data) {
+    int int_address = hex_to_int(hex_address);
+    int x = int_address / 8;
+    int y = int_address % 8;
+    mem[int_to_hex(x * 8)][y] = data;
+}
+
 map<int, vector<tuple<int, int, vector<string>>>> cache_init() {
     NUM_SETS = (CACHE_SIZE / BLOCK_SIZE) / ASSOCIATIVITY;
 
@@ -78,7 +88,24 @@ map<int, vector<tuple<int, int, vector<string>>>> cache_init() {
     return cache_mem;
 }
 
-void retrieve_from_cache(
+void print_cache(map<int, vector<tuple<int, int, vector<string>>>>& cache) {
+    for (int i = 0; i < (int)pow(2, INDEX_WIDTH); i++) {
+        cout << "Index " << i << endl;
+        for (auto x : cache[i]) {
+            int tag, dirty;
+            vector<string> data;
+            tie(tag, dirty, data) = x;
+            cout<<tag<<" "<<dirty<<" ";
+            for(auto y:data){
+                cout<<y<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+}
+
+tuple<int, int, vector<string>>* retrieve_from_cache(
     map<int, vector<tuple<int, int, vector<string>>>>& cache,
     map<string, vector<string>>& main_memory, string target_address) {
     int target_address_int = hex_to_int(target_address);
@@ -87,8 +114,8 @@ void retrieve_from_cache(
     int index = (target_address_int / BLOCK_SIZE) % NUM_SETS;
 
     int tag = target_address_int / (int)pow(2, INDEX_WIDTH + OFFSET_WIDTH);
-    
-    // get<0> = Tag get<1> = Dirty get<2> = Data Vector
+
+    // get<0> = Tag | get<1> = Dirty | get<2> = Data Vector
     int i = 0;
     for (auto x : cache[index]) {
         if (get<0>(x) == -1) {
@@ -96,18 +123,78 @@ void retrieve_from_cache(
                  << endl;
             cache[index][i] = make_tuple(
                 tag, 0, retrieve_from_mem(main_memory, target_address));
-            return;
+            return &cache[index][i];
         } else if (get<0>(x) == tag) {
-            cout<<"Read hit"<<endl;
-            cout<<"Data at 0x"<<target_address<<" is "<<get<2>(x)[offset]<<endl;
-            return;
+            cout << "Read hit" << endl;
+            cout << "Data at 0x" << target_address << " is "
+                 << get<2>(x)[offset] << endl;
+            return &cache[index][i];
         }
         i++;
     }
     cout << endl;
     cout << "Cache miss. Retrieving and loading from main memory" << endl;
-    cache[index][rand() % ASSOCIATIVITY] =
+
+    int temp = rand() % ASSOCIATIVITY;
+    cache[index][temp] =
         make_tuple(tag, 0, retrieve_from_mem(main_memory, target_address));
+    return &cache[index][temp];
+}
+
+tuple<int, int, vector<string>>* read_from_cache(
+    map<int, vector<tuple<int, int, vector<string>>>>& cache,
+    map<string, vector<string>>& main_memory, string target_address) {
+    int target_address_int = hex_to_int(target_address);
+    int offset = target_address_int % BLOCK_SIZE;
+
+    int index = (target_address_int / BLOCK_SIZE) % NUM_SETS;
+
+    int tag = target_address_int / (int)pow(2, INDEX_WIDTH + OFFSET_WIDTH);
+
+    // get<0> = Tag | get<1> = Dirty | get<2> = Data Vector
+    int i = 0;
+    for (auto x : cache[index]) {
+        if (get<0>(x) == -1) {
+            cout << "Cache miss. Retrieving and loading from main memory"
+                 << endl;
+            cache[index][i] = make_tuple(
+                tag, 0, retrieve_from_mem(main_memory, target_address));
+            return &cache[index][i];
+        } else if (get<0>(x) == tag) {
+            cout << "Read hit" << endl;
+            cout << "Data at 0x" << target_address << " is "
+                 << get<2>(x)[offset] << endl;
+            return &cache[index][i];
+        }
+        i++;
+    }
+    return NULL;
+}
+
+void write_through(map<int, vector<tuple<int, int, vector<string>>>>& cache,
+                   map<string, vector<string>>& main_memory,
+                   string target_address, string new_data) {
+    int target_address_int = hex_to_int(target_address);
+    int offset = target_address_int % BLOCK_SIZE;
+
+    int index = (target_address_int / BLOCK_SIZE) % NUM_SETS;
+
+    int tag = target_address_int / (int)pow(2, INDEX_WIDTH + OFFSET_WIDTH);
+
+    if (WRITE_MISS) {
+        auto *x = retrieve_from_cache(cache, main_memory, target_address);
+        get<2>(*x)[offset] = new_data;
+        
+    } else {
+        auto *x = read_from_cache(cache, main_memory, target_address);
+        if (x) {
+            get<2>(*x)[offset] = new_data;
+        }
+    }
+    write_to_mem(main_memory, target_address, new_data);
+    cout<<endl;
+    print_cache(cache);
+    cout<<endl;
 }
 
 int main() {
@@ -126,6 +213,12 @@ int main() {
     cout << "Enter ASSOCIATIVITY: ";
     cin >> ASSOCIATIVITY;
 
+    cout << "Write back(0) / Write through (1): ";
+    cin >> WRITE_HIT;
+
+    cout << "Allocate (1) / No Allocate (0): ";
+    cin >> WRITE_MISS;
+
     map<int, vector<tuple<int, int, vector<string>>>> cache_mem = cache_init();
 
     print_mem(main_memory);
@@ -139,6 +232,25 @@ int main() {
             break;
         }
         retrieve_from_cache(cache_mem, main_memory, target_address);
+    }
+    print_cache(cache_mem);
+    while (true) {
+        string target_address;
+        string input_byte;
+
+        cout << "Enter address to write ";
+        cin >> target_address;
+
+        cout << "Enter byte value to write (in hex): ";
+        cin >> input_byte;
+
+        if (target_address == "q") {
+            break;
+        }
+        if (WRITE_HIT) {
+            write_through(cache_mem, main_memory, target_address, input_byte);
+        } else {
+        }
     }
     cout << "pass" << endl;
 }
